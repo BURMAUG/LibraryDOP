02c3d50e958b622b88f6463aefd6d5924f239434
Burmau Garba <burmaud1@gmail.com>
2024-03-02 00:13:15 -0500

    Library updated.

---
+++ src/main/java/org/example/dataentity/Library.java
@@ -0,0 +1,15 @@
+package org.example.dataentity;
+
+public class Library {
+    public static void searchBook(){}
+    public static void addBookItem(){}
+    public static boolean isBlocked(){
+        return false;
+    }
+    public static boolean isMember(){
+        return false;
+    }
+    public static void getBookLendings(){}
+    public static void checkBook(){}
+    public static void returnBook(){}
+}
